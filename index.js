c554c9d1b8b1521f03855954635f4d8ab6e88256
Angelina Bogush <angelinabogush@yandex.ru>
2023-05-16 13:27:43 +0300

    адаптив, readMe

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const cardContainerCactuses = page.querySelector(
 const cardContainerOrhideys = page.querySelector(
   ".category__container_orhideys"
 );
-let screenWidth = window.innerWidth;
 const header = page.querySelector('.header');
 const headerLinks = page.querySelectorAll('.header__link');
 const categoriesLinks = page.querySelectorAll('.categories__link');
@@ -26,188 +25,151 @@ const toggleButtonText = page.querySelector('.header__theme-text');
 const headerBag = page.querySelector('.header__bag');
 const menuButton = page.querySelector('.header__menu-button');
 const menu = page.querySelector('.header__list');
-
+//открытие карточки при покупки
 const openPopup = function () {
   popup.classList.add("popup_opened");
 };
 const closePopup = function () {
   popup.classList.remove("popup_opened");
 };
+//функция даты
+ function getDayInfo(dateString) {
+   const date = new Date(dateString);
+   const options = {
+     weekday: "long",
+     week: "numeric",
+     month: "long",
+     year: "numeric",
+   };
+   const formatter = new Intl.DateTimeFormat("ru-RU", options);
+   const formattedDate = formatter.format(date);
+   const parts = formattedDate.split(" ");
+   const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
+   const firstMondayOfMonth = new Date(firstDayOfMonth);
+   firstMondayOfMonth.setDate(
+     firstMondayOfMonth.getDate() + ((1 - firstDayOfMonth.getDay() + 7) % 7)
+   );
+   const weekNumber =
+     Math.floor((date.getDate() - firstMondayOfMonth.getDate()) / 7) + 1;
+   const monthsGenitive = [
+     "января",
+     "февраля",
+     "марта",
+     "апреля",
+     "мая",
+     "июня",
+     "июля",
+     "августа",
+     "сентября",
+     "октября",
+     "ноября",
+     "декабря",
+   ];
+   const monthIndex = date.getMonth();
+   const monthGenitive = monthsGenitive[monthIndex];
+   const result = `${parts[3]}, ${weekNumber} неделя ${monthGenitive} ${parts[1]} года`;
+   return result;
+ }
+//редактирую дату под стандарт ISO
+ const reverseDate = function (arrayData) {
+   return arrayData.split(".").reverse().join("-");
+ };
+ //функция открытия карточки при клике
+ const clickCard = function (arrayLink, arrayName, arrayPrice, arrayDate) {
+   popupImage.src = arrayLink;
+   popupImage.alt = arrayName;
+   popupTitle.textContent = arrayName;
+   popupPrice.textContent = `Стоимость:  ${arrayPrice}`;
+   popupDate.textContent = getDayInfo(arrayDate);
+   openPopup();
+ };
 
-//   function getDayInfo(dateString) {
-//     const date = new Date(dateString);
-//     const dayOfWeek = getDayOfWeek(date.getDay());
-//     const weekNumber = getMonthWeekNumber(date);
-//     const month = getMonthName(date.getMonth());
-//     const year = date.getFullYear();
-//      return `${dayOfWeek}, ${weekNumber} неделя ${month} ${year} года`;
-//   }
-//   function getDayOfWeek(day) {
-//     const daysOfWeek = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
-//     return daysOfWeek[day];
-//   }
-  
-//   function getMonthWeekNumber(date) {
-//     const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
-//     const firstMondayOfMonth = new Date(firstDayOfMonth);
-//     firstMondayOfMonth.setDate(firstMondayOfMonth.getDate() + ((1 - firstDayOfMonth.getDay() + 7) % 7));
-//     const weekNumber = Math.floor((date.getDate() - firstMondayOfMonth.getDate()) / 7) + 1;
-//     return weekNumber;
-//   }
-  
-  
-//   function getMonthName(month) {
-//     const monthNames = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня",
-//                       "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"];
-//     return monthNames[month];
-//   }
-  
-     function getDayInfo(dateString) {
-       const date = new Date(dateString);
-       const options = {
-         weekday: "long",
-         week: "numeric",
-         month: "long",
-         year: "numeric",
-       };
-       const formatter = new Intl.DateTimeFormat("ru-RU", options);
-       const formattedDate = formatter.format(date);
-       const parts = formattedDate.split(" ");
-       const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
-       const firstMondayOfMonth = new Date(firstDayOfMonth);
-       firstMondayOfMonth.setDate(
-         firstMondayOfMonth.getDate() + ((1 - firstDayOfMonth.getDay() + 7) % 7)
-       );
-       const weekNumber =
-         Math.floor((date.getDate() - firstMondayOfMonth.getDate()) / 7) + 1;
-       const monthsGenitive = [
-         "января",
-         "февраля",
-         "марта",
-         "апреля",
-         "мая",
-         "июня",
-         "июля",
-         "августа",
-         "сентября",
-         "октября",
-         "ноября",
-         "декабря",
-       ];
-       const monthIndex = date.getMonth();
-       const monthGenitive = monthsGenitive[monthIndex];
-       const result = `${parts[3]}, ${weekNumber} неделя ${monthGenitive} ${parts[1]} года`;
-       return result;
-     }
+ const createCard = function (array) {
+   //создание новой карточки
+   const newCard = cardTemplate.querySelector(".card").cloneNode(true);
+   const newCardName = newCard.querySelector(".card__name");
+   const newCardPrice = newCard.querySelector(".card__price");
+   const newCardImage = newCard.querySelector(".card__image");
+   const newCardDate = newCard.querySelector(".card__date");
+   const newCardButton = newCard.querySelector(".card__button");
+   newCardName.textContent = array["name"];
+   newCardImage.src = array["link"];
+   newCardImage.alt = array["name"];
+   newCardDate.textContent = getDayInfo(reverseDate(array["data"]));
+   newCardPrice.textContent = array["price"];
 
-const reverseDate = function(arrayData){
-return arrayData.split('.').reverse().join('-')
-}
-const clickCard = function (arrayLink, arrayName, arrayPrice, arrayDate) {
-    popupImage.src = arrayLink;
-    popupImage.alt = arrayName;
-    popupTitle.textContent = arrayName;
-    popupPrice.textContent = `Стоимость:  ${arrayPrice}`;
-    popupDate.textContent = getDayInfo(arrayDate);
-    openPopup();
+   newCardButton.addEventListener("click", function () {
+     clickCard(array["link"], array["name"], array["price"], array["data"]);
+   });
+   newCardImage.addEventListener("click", function () {
+     clickCard(array["link"], array["name"], array["price"], array["data"]);
+   });
+   return newCard;
+ };
+//добавление новой карточки в категорию
+ const addCard = function (array, container) {
+   const newCard = createCard(array);
+   container.append(newCard);
+ };
+//перебор массивов для выведения карточек
+ arrayCactuses.forEach((info) => {
+   addCard(info, cardContainerCactuses);
+ });
+ arrayOrchideys.forEach((info) => {
+   addCard(info, cardContainerOrhideys);
+ });
+ arrayPalms.forEach((info) => {
+   addCard(info, cardContainerPalms);
+ });
+ arrayBonsai.forEach((info) => {
+   addCard(info, cardContainerBonsai);
+ });
 
-  };
+ //закрытие карточки
+ popupCloseIcon.addEventListener("click", () => {
+   closePopup();
+ });
+ //кнопка купить
+ const buyItem = function () {
+   closePopup();
+   alert("Поздравляем с покупкой!");
+   popupForm.reset();
+ };
+ popupForm.addEventListener("submit", buyItem);
 
-const createCard = function (array) {
-                                                                       //создание новой карточки
-  const newCard = cardTemplate.querySelector(".card").cloneNode(true);
-  const newCardName = newCard.querySelector(".card__name");
-  const newCardPrice = newCard.querySelector(".card__price");
-  const newCardImage = newCard.querySelector(".card__image");
-  const newCardDate = newCard.querySelector(".card__date");
-  const newCardButton = newCard.querySelector(".card__button");
-  newCardName.textContent = array["name"];
-  newCardImage.src = array["link"];
-  newCardImage.alt = array["name"];
-  newCardDate.textContent = getDayInfo(reverseDate(array['data']));
-  newCardPrice.textContent = array["price"];
+ //кнопка скролла наверх
+ const showButtonUp = function () {
+   buttonUp.classList.add("button-up_active");
+ };
 
-  newCardButton.addEventListener("click", function () {
-    clickCard(array["link"], array["name"], array["price"], array["data"]);
-  });
-  newCardImage.addEventListener("click", function () {
-    clickCard(array["link"], array["name"], array["price"], array["data"]);
-  });
-  return newCard;
-};
+ const hideButtonUp = function () {
+   buttonUp.classList.remove("button-up_active");
+ };
+ window.addEventListener("scroll", () => {
+   const scrollY = window.scrollY;
+   scrollY > 300 ? showButtonUp() : hideButtonUp();
+ });
+ buttonUp.addEventListener("click", () => {
+   window.scrollTo({
+     top: 0,
+     left: 0,
+     behavior: "smooth",
+   });
+ });
+// кнопка смены темы
+ const changeThemeDark = function () {
+   page.classList.toggle("dark");
+   buttonUp.classList.toggle("button-up_dark");
+   header.classList.toggle("dark");
+   headerLinks.forEach((link) => {
+     link.classList.toggle("dark");
+   });
+   toggleButtonText.classList.toggle("dark");
+   headerBag.classList.toggle("dark");
+   categoriesLinks.forEach((link) => {
+     link.classList.toggle("dark");
+   });
+ };
+ toggleButton.addEventListener("click", changeThemeDark);
 
-const addCard = function (array, container) {
-  //добавление карточек товаров в секции
-  const newCard = createCard(array);
-  container.append(newCard);
-};
-
-arrayCactuses.forEach((info) => {
-  addCard(info, cardContainerCactuses);
-});
-arrayOrchideys.forEach((info) => {
-  addCard(info, cardContainerOrhideys);
-});
-arrayPalms.forEach((info) => {
-  addCard(info, cardContainerPalms);
-});
-arrayBonsai.forEach((info) => {
-  addCard(info, cardContainerBonsai);
-});
-
-//закрытие карточки на крестик
-popupCloseIcon.addEventListener("click", () => {
-  closePopup();
-});                             
-//кнопка купить
-const buyItem = function(){
-closePopup();
-alert('Поздравляем с покупкой!');
-popupForm.reset();
-}
-popupForm.addEventListener('submit', buyItem)
-
-//кнопка скролла наверх
-const showButtonUp = function () {
-  buttonUp.classList.add("button-up_active"); 
-};
-
-const hideButtonUp = function () {
-  buttonUp.classList.remove("button-up_active"); 
-};
-window.addEventListener('scroll', () => {
-    const scrollY = window.scrollY;
-    scrollY > 300 ? showButtonUp() : hideButtonUp();
-  });
-buttonUp.addEventListener('click', () => {
-    window.scrollTo({
-        top:0,
-        left: 0,
-        behavior: 'smooth'
-    }
-    )
-})                                                
-
- const changeThemeDark = function(){
-        page.classList.toggle('dark');
-        buttonUp.classList.toggle('button-up_dark');
-        header.classList.toggle('dark');
-        headerLinks.forEach((link) => {
-            link.classList.toggle('dark');})
-        toggleButtonText.classList.toggle('dark');
-        headerBag.classList.toggle('dark');
-        categoriesLinks.forEach((link) => {
-            link.classList.toggle('dark')
-        })
-        }
-toggleButton.addEventListener('click', changeThemeDark);
-
-menuButton.addEventListener('click', function() {
- if (menu.style.display === 'flex' && screenWidth <= 768) {
-      menu.style.display = 'none';
-    }
-    else {
-      menu.style.display = 'flex';
-    }
-});
   
